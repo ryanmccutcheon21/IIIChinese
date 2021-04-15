5701e6db0d27fe97ed29851b810fc173f0205efb
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-04-15 08:42:01 -0500

    Navbar styles cleanup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './components/Navbar'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { GlobalStyle } from './globalStyles';
 
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <NavBar />
     </Router>
   );
