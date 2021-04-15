db03d9f4e098c31d596344642686705afcd55b13
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-04-15 10:33:07 -0500

    Created Dessert Section

---
+++ src/components/Feature/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FeaturedContainer, FeatureButton } from './FeatureElements'
+
+const Feature = () => {
+    return (
+        <FeaturedContainer>
+            <h1>Dish of the Day</h1>
+            <p>Sweet and Sour Chicken, with rice or lo mein noodles and 1 egg roll</p>
+            <FeatureButton>Order Now</FeatureButton>
+        </FeaturedContainer>
+    )
+}
+
+export default Feature
--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,6 +1,10 @@
 import product1 from '../../images/product-1.jpg'
 import product2 from '../../images/product-2.jpg'
 import product3 from '../../images/product-3.jpg'
+import dessert1 from '../../images/dessert-1.jpg'
+import dessert2 from '../../images/dessert-2.jpg'
+import dessert3 from '../../images/dessert-3.jpg'
+import dessert4 from '../../images/dessert-4.jpg'
 
 export const productData = [
     {
@@ -23,6 +27,41 @@ export const productData = [
         img: product1,
         alt: 'Dinner',
         name: 'Dinner',
+        desc: 'Our famous lo mein noodles with a delicous array of vegetables',
+        price: '$9.99',
+        button: 'Add to Cart'
+    }
+]
+
+export const productDataTwo = [
+    {
+        img: dessert1,
+        alt: "S'mores egg rolls",
+        name: "S'mores",
+        desc: 'Our famous lo mein noodles with a delicous array of vegetables',
+        price: '$9.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: dessert2,
+        alt: 'Cheesecake',
+        name: 'Cheesecake',
+        desc: 'Our famous lo mein noodles with a delicous array of vegetables',
+        price: '$9.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: dessert3,
+        alt: 'Oreo',
+        name: 'Oreo',
+        desc: 'Our famous lo mein noodles with a delicous array of vegetables',
+        price: '$9.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: dessert4,
+        alt: 'Snickers',
+        name: 'Snickers',
         desc: 'Our famous lo mein noodles with a delicous array of vegetables',
         price: '$9.99',
         button: 'Add to Cart'
