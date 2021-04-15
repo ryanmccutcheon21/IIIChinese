e1a48bf1aa11b5b347da176956da32ed16562bd7
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2021-04-15 10:10:42 -0500

    Created Products Section

---
+++ src/components/Products/data.js
@@ -0,0 +1,30 @@
+import product1 from '../../images/product-1.jpg'
+import product2 from '../../images/product-2.jpg'
+import product3 from '../../images/product-3.jpg'
+
+export const productData = [
+    {
+        img: product3,
+        alt: 'Appetizers',
+        name: 'Appetizers',
+        desc: 'Our famous lo mein noodles with a delicous array of vegetables',
+        price: '$9.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: product2,
+        alt: 'Lunch',
+        name: 'Lunch',
+        desc: 'Our famous lo mein noodles with a delicous array of vegetables',
+        price: '$9.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: product1,
+        alt: 'Dinner',
+        name: 'Dinner',
+        desc: 'Our famous lo mein noodles with a delicous array of vegetables',
+        price: '$9.99',
+        button: 'Add to Cart'
+    }
+]
